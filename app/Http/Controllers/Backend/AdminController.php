8996d9eba59201d30f8da0809927f6274529657d
dev-sajid007 <dev.sajid007@gmail.com>
2024-03-31 00:10:05 +0600

    Admin Login Complete

--- app/Http/Controllers/Backend/AdminController.php
+++ app/Http/Controllers/Backend/AdminController.php
@@ -8,6 +8,13 @@ use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
+
+
+    public function login()
+    {
+        return view('admin.auth.login');
+    }
+
     public function dashboard(){
         return view('admin.dashboard');
     }
--- routes/web.php
+++ routes/web.php
@@ -27,25 +27,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[HomeController::class,'index'])->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
 require __DIR__ . '/auth.php';
 
-
+Route::get('/login', [AdminController::class, 'login'])->name('login');
 
 Route::middleware(['auth','role:admin'])->group(function () {
 
 
     Route::group(['as'=>'admin.','prefix'=>'admin'],function(){
 
+      
         Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
         Route::get('/logout', [AdminController::class, 'logout'])->name('logout');
     
