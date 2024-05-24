1d154f1fa7c09b79c12f77a9e55d270e4283a19e
MH Munna <Monirhasanmunna1@gmail.com>
2024-05-24 22:45:14 +0600

    administrative overlay remove

--- resources/views/frontend/page/administrative.blade.php
+++ resources/views/frontend/page/administrative.blade.php
@@ -10,16 +10,6 @@
                         <div class="single-item">
                             <div class="img-holder">
                                 <figure><img src="{{asset(@$data->photo)}}" alt=""></figure>
-                                <div class="overlay">
-                                    <div class="box">
-                                        <div class="content">
-                                            <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                                            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                                            <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                                            <a href="#"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a>
-                                        </div>
-                                    </div>
-                                </div>
                             </div>
                             <div class="lower-content">
                                 <h4><a href="single-volunteer.html">{{@$data->name}}</a></h4>
