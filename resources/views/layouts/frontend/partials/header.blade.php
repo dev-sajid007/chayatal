f804136724ad90eaf1a26953aeef9d635e8696ad
dev-sajid007 <dev.sajid007@gmail.com>
2024-06-05 15:51:45 +0600

    Menu linked

--- resources/views/layouts/frontend/partials/header.blade.php
+++ resources/views/layouts/frontend/partials/header.blade.php
@@ -46,7 +46,7 @@
                                 <ul class="navigation clearfix">
 
                                     @foreach ($menus as $menu)
-                                    <li class="{{count($menu->childs) > 0 ? "dropdown" : ""}}"><a href="#">{{$menu->title}}</a>
+                                    <li class="{{count($menu->childs) > 0 ? "dropdown" : ""}}"><a href="{{$menu->url}}">{{$menu->title}}</a>
                                         @if ($menu->childs)
                                         <ul>
                                             @foreach ($menu->childs as $item)
